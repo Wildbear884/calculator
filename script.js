8f162bf550b0372ffee23f92d118bed60efcc7ea
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-03-13 12:38:23 -0400

    Make operator and equal buttons functional
    And tested to make sure they work and laid some groundwork for the
    decimal, clear, and backspace button.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(term1, term2) {
-  return term1 + term2;
+  return Number(term1) + Number(term2);
 }
 
 function subtract(term1, term2) {
@@ -11,7 +11,7 @@ function multiply(factor1, factor2) {
 }
 
 function divide(dividend, divisor) {
-  if (divisor === 0) {
+  if (Number(divisor) === 0) {
     return "Can't divide by 0";
   }
   return dividend / divisor;
@@ -31,6 +31,137 @@ function operate(term1, term2, operator) {
   }
 }
 
-let calcDisplayTerm1 = 0;
-let calcDisplayTerm2 = 0;
-let calcDisplayAnswer = 0;
+function getCalcBtnClicked(btn) {
+  let btnDetails = {
+    isDigit: false,
+    isOperator: false,
+    isBackspace: false,
+    isClear: false,
+    isEquals: false,
+    content: "",
+  };
+  
+  switch (btn) {
+    case calcBtns[0]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "7";
+      break;
+    case calcBtns[1]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "8";
+      break;
+    case calcBtns[2]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "9";
+      break;
+    case calcBtns[3]:
+      btnDetails.isOperator = true;
+      btnDetails.content = "divide";
+      break;
+    case calcBtns[4]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "4";
+      break;
+    case calcBtns[5]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "5";
+      break;
+    case calcBtns[6]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "6";
+      break;
+    case calcBtns[7]:
+      btnDetails.isOperator = true;
+      btnDetails.content = "multiply";
+      break;
+    case calcBtns[8]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "1";
+      break;
+    case calcBtns[9]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "2";
+      break;
+    case calcBtns[10]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "3";
+      break;
+    case calcBtns[11]:
+      btnDetails.isOperator = true;
+      btnDetails.content = "subtract";
+      break;
+    case calcBtns[12]:
+      btnDetails.isDigit = true;
+      btnDetails.content = ".";
+      break;
+    case calcBtns[13]:
+      btnDetails.isDigit = true;
+      btnDetails.content = "0";
+      break;
+    case calcBtns[14]:
+      btnDetails.isBackspace = true;
+      btnDetails.content = "backspace";
+      break;
+    case calcBtns[15]:
+      btnDetails.isOperator = true;
+      btnDetails.content = "add";
+      break;
+    case calcBtns[16]:
+      btnDetails.isClear = true;
+      btnDetails.content = "clear";
+      break;
+    case calcBtns[17]:
+      btnDetails.isEquals = true;
+      btnDetails.content = "equals";
+      break;
+    default:
+      return "Unknown button";
+  }
+  return btnDetails;
+}
+
+function calcBtnClicked() {
+  const btn = getCalcBtnClicked(this);
+  
+  if (btn.isDigit && !operatorSelected) {
+    firstTerm += btn.content;
+    calcDisplay.textContent = firstTerm;
+  } else if (btn.isDigit && operatorSelected) {
+    secondTerm += btn.content;
+    calcDisplay.textContent = secondTerm;
+  }
+  
+  if (btn.isOperator && !operatorSelected && firstTerm.length > 0) {
+    operatorSelected = true;
+  } else if (btn.isOperator && operatorSelected && secondTerm.length > 0) {
+    answer = operate(firstTerm, secondTerm, selectedOperator).toString();
+    calcDisplay.textContent = answer;
+    firstTerm = answer;
+    secondTerm = "";
+  }
+
+  if (btn.isOperator) {
+    selectedOperator = btn.content;
+  } 
+  
+  if (btn.isEquals && operatorSelected && firstTerm.length > 0 && secondTerm.length > 0) {
+    answer = operate(firstTerm, secondTerm, selectedOperator);
+    calcDisplay.textContent = answer;
+    firstTerm = "";
+    secondTerm = "";
+    operatorSelected = false;
+  }
+}
+
+let selectedOperator = "";
+let operatorSelected = false;
+let firstTerm = "";
+let secondTerm = "";
+let answer = "";
+
+const calcBtns = document.querySelectorAll(".calc-buttons-container > button");
+const calcDisplay = document.querySelector(".calc-display");
+
+calcBtns.forEach(btn => {
+  btn.addEventListener("click", calcBtnClicked);
+})
